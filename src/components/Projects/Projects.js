253c01a186e042d1e322db64b3db289b1c7a274d
KaustubhShelar <81356206+KaustubhShelar@users.noreply.github.com>
2023-04-29 00:51:11 +0530

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -52,7 +52,7 @@ function Projects() {
                     </div>
                     <div className="projects--body">
                         <div className="projects--bodyContainer">
-                            {projectsData.slice(0, 3).map(project => (
+                            {projectsData.slice(0, 4).map(project => (
                                 <SingleProject
                                     theme={theme}
                                     key={project.id}
@@ -67,7 +67,7 @@ function Projects() {
                             ))}
                         </div> 
 
-                        {projectsData.length > 3 && (
+                        {projectsData.length > 4 && (
                             <div className="projects--viewAll">
                                 <Link to="/projects">
                                     <button className={classes.viewAllBtn}>
