b5eff6c76c02741df848a10ac1dba57202bd3c38
KaustubhShelar <81356206+KaustubhShelar@users.noreply.github.com>
2023-04-28 19:28:54 +0530

    Update headerData.js

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,9 +1,9 @@
 import resume from '../assets/pdf/resume.pdf'
 
 export const headerData = {
-    name: 'Jane Doe',
-    title: "Web Developer",
+    name: 'Kaustubh Shelar',
+    title: "Software Engineer",
     desciption:"There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
-    image: 'https://raw.githubusercontent.com/PhantomScript/asset-container/main/developer-portfolio/landingImg.png',
+    image: 'https://drive.google.com/file/d/1K3zEMkXT_f7XdRV2eYQWR2yFK6Y1B4de/view?usp=sharing',
     resumePdf: resume
 }
