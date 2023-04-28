1454226172657d4229646fe80af1a8f27373ec68
KaustubhShelar <81356206+KaustubhShelar@users.noreply.github.com>
2023-04-28 22:12:19 +0530

    Update educationData.js

--- src/data/educationData.js
+++ src/data/educationData.js
@@ -1,16 +1,16 @@
 export const educationData = [
     {
         id: 1,
-        institution: 'Marble Hills Middle School',
-        course: 'Higher Secondary Education',
-        startYear: '2007',
-        endYear: '2015'
+        institution: 'MIT Academy of Engineering',
+        course: 'B.Tech-Computer Engineering',
+        startYear: '2019',
+        endYear: 'Present'
     },
     {
         id: 2,
-        institution: 'Somerset School of Science',
-        course: 'Bachelor of Technology',
-        startYear: '2015',
+        institution: 'Matoshri Junior College',
+        course: 'HSC',
+        startYear: '2017',
         endYear: '2019'
     },
     {
