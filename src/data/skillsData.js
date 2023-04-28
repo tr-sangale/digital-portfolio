224fd54f24ca4d704f4a1e120bd4ab9e984bdf2d
KaustubhShelar <81356206+KaustubhShelar@users.noreply.github.com>
2023-04-28 22:47:27 +0530

    Update skillsData.js

--- src/data/skillsData.js
+++ src/data/skillsData.js
@@ -14,6 +14,11 @@
     'MySQL',
     'MaterialUI',
     'Numpy',
+    'Hadoop',
+    'Excel',
+    'Github',
+    'Tableau',
+    'PowerBI'
 ]
 
 // Choose your skills from below. Make sure it's in the same format and spelled correctly.
