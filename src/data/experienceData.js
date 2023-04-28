32456d427547445fb981c4484e672fbf67871011
KaustubhShelar <81356206+KaustubhShelar@users.noreply.github.com>
2023-04-28 23:17:15 +0530

    Update experienceData.js

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,23 +1,18 @@
 export const experienceData = [
     {
         id: 1,
-        company: 'Globex Corporation',
-        jobtitle: 'Frontend Developer',
-        startYear: '2018',
-        endYear: '2019'
+        company: 'SteepGraph Systemes',
+        jobtitle: 'Software Engineer',
+        startYear: 'Jan\'2023',
+        endYear: 'Present',
+        description: '● Installed and configured the 3DEXPERIENCE software by Dassault Systemes\n● Worked on a live project for the company\n● Fetched 3DX data and migrated it to H2 Database and used this data to generate Data Assessment Report in PDF format using JPO(Java) code'
     },
     {
         id: 2,
-        company: 'Vehement Capital Partners',
-        jobtitle: 'Backend Developer',
-        startYear: '2019',
-        endYear: '2020'
-    },
-    {
-        id: 3,
-        company: 'Wonka Industries',
-        jobtitle: 'Marketing',
-        startYear: '2020',
-        endYear: 'Present'
+        company: 'UInSports',
+        jobtitle: 'Web Developer',
+        startYear: 'Sep\'2021',
+        endYear: 'jul\'2022',
+        description: '● Created a feature as a react component for the client website, which will crop/edit an image before posting or setting up the profile picture\n● Market Research and exploring new features for the Web Application based on current trends in technology and sports\n● Analyzed best practices and project structure in Flutter for mobile app development according to company requirements\n● Technologies used are HTML, CSS, ReactJs, Material UI, Python Flask, Flutter\n● Followed Agile Scrum methodology throughout the internship'
     },
 ]
