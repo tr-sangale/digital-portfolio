470cd339ff26fcb8af72b04f9cc3aa913364fe1e
KaustubhShelar <81356206+KaustubhShelar@users.noreply.github.com>
2023-04-28 22:22:36 +0530

    Update educationData.js

--- src/data/educationData.js
+++ src/data/educationData.js
@@ -4,13 +4,15 @@ export const educationData = [
         institution: 'MIT Academy of Engineering',
         course: 'B.Tech-Computer Engineering',
         startYear: '2019',
-        endYear: 'Present'
+        endYear: 'Present',
+        grade: '8.95/10.00'
     },
     {
         id: 2,
         institution: 'Matoshri Junior College',
         course: 'HSC',
         startYear: '2017',
-        endYear: '2019'
+        endYear: '2019',
+        grade: '65.08/100.00'
     },
 ]
