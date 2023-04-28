e9ef5deda0353350e63f17ef05dca33e836ccbe2
KaustubhShelar <81356206+KaustubhShelar@users.noreply.github.com>
2023-04-29 01:04:50 +0530

    Update contactsData.js

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -1,7 +1,7 @@
 export const contactsData = {
-    email: 'janedoe.test@gmail.com',
-    phone: '+915588776600',
-    address: 'Menlo Park, California, United States - 673822 ',
+    email: 'kaustubh8shelar@gmail.com',
+    phone: '+917666745559',
+    address: 'Pune, Maharashtra',
 
     sheetAPI: ''
 }
