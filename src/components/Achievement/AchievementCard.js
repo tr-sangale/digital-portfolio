5e86078748fefaf6bba6a669b27856131cc4c6fe
KaustubhShelar <81356206+KaustubhShelar@users.noreply.github.com>
2023-05-01 23:18:12 +0530

    Update AchievementCard.js

--- src/components/Achievement/AchievementCard.js
+++ src/components/Achievement/AchievementCard.js
@@ -28,12 +28,6 @@ function AchievementCard({id, title, details, date, field, image}) {
                         <h2 style={{color: theme.tertiary}}>{title}</h2>
                         <p style={{color: theme.tertiary80}}>{details}</p>
                     </div>
-                    <div className="achievecard-details2" style={{color: theme.primary}}>
-                        <h5>{date}</h5>
-                        <div className="achievecard-field">
-                            <h5>{field}</h5>
-                        </div>   
-                    </div>
                 </div> 
                 <div className="achievecard-imgcontainer">
                     <img src={image} alt="" />
