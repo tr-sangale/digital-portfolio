ad7a6c2691cf6e6cab0df21548c1330d3e8387d1
KaustubhShelar <81356206+KaustubhShelar@users.noreply.github.com>
2023-05-01 23:13:54 +0530

    Update AchievementCard.js

--- src/components/Achievement/AchievementCard.js
+++ src/components/Achievement/AchievementCard.js
@@ -4,8 +4,6 @@ import Fade from 'react-reveal/Fade';
 
 import { ThemeContext } from '../../contexts/ThemeContext';
 
-import { AiOutlineFolder } from "react-icons/ai";
-
 import './Achievement.css'
 
 function AchievementCard({id, title, details, date, field, image}) {
@@ -33,7 +31,6 @@ function AchievementCard({id, title, details, date, field, image}) {
                     <div className="achievecard-details2" style={{color: theme.primary}}>
                         <h5>{date}</h5>
                         <div className="achievecard-field">
-                            <AiOutlineFolder />
                             <h5>{field}</h5>
                         </div>   
                     </div>
