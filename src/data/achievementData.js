11c19df96c6abf968738d8c320e8b7cf545b620b
KaustubhShelar <81356206+KaustubhShelar@users.noreply.github.com>
2023-05-01 23:33:14 +0530

    Update achievementData.js

--- src/data/achievementData.js
+++ src/data/achievementData.js
@@ -24,7 +24,7 @@ export const achievementData = {
             date : '',
             field : '',
             image : 'https://images.unsplash.com/photo-1489389944381-3471b5b30f04?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
-        }
+        },
         {
             id : 4,
             title : 'Microsoft Certified: Azure Data Fundamentals, Azure AI Fundamentals',
