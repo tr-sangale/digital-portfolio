be28efa7acc9715176293ff259bfe8e0ab5e22a6
tr-sangale <86203327+tr-sangale@users.noreply.github.com>
2023-05-04 01:53:41 +0530

    Update Main.js

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
 
-import { Navbar, Footer, Landing, About, Skills, Education, Experience, Contacts, Projects, Achievement } from '../../components'
+import { Navbar, Footer, Landing, About, Skills,Blog, Education, Experience, Contacts, Projects, Achievement } from '../../components'
 import { headerData } from '../../data/headerData'
 
 function Main() {
