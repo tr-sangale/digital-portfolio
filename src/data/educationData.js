9596d554436074a7ba2858a59950c84339fab5a0
tr-sangale <86203327+tr-sangale@users.noreply.github.com>
2023-05-04 02:09:48 +0530

    Update educationData.js

--- src/data/educationData.js
+++ src/data/educationData.js
@@ -2,17 +2,25 @@ export const educationData = [
     {
         id: 1,
         institution: 'MIT Academy of Engineering',
-        course: 'B.Tech-Computer Engineering',
+        course: 'B.Tech-Electronics and Telecommunication',
         startYear: '2019',
         endYear: 'Present',
-        grade: '8.95/10.00'
+        grade: '8.75/10.00'
     },
     {
         id: 2,
-        institution: 'Matoshri Junior College',
+        institution: 'Akalank Public School',
         course: 'HSC',
         startYear: '2017',
         endYear: '2019',
-        grade: '65.08/100.00'
+        grade: '69.00/100.00'
+    },
+    {
+        id: 3,
+        institution: 'Army Public School',
+        course: 'SSC',
+        startYear: '2016',
+        endYear: '2017',
+        grade: '95.00/100.00'
     },
 ]
