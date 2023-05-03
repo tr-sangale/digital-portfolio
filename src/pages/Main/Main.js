cd38bbe1fe8310fb6e4d56c3e80633caa22f4204
tr-sangale <86203327+tr-sangale@users.noreply.github.com>
2023-05-04 01:52:40 +0530

    Update Main.js

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -19,6 +19,7 @@ function Main() {
             <Experience />
             <Projects />
             <Achievement />
+            <Blog/>
             <Contacts />
             <Footer />
         </div>
