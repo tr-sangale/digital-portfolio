94460d30f4e6f647e5037a7745c80337077f71d3
tr-sangale <86203327+tr-sangale@users.noreply.github.com>
2023-05-04 01:22:19 +0530

    Update aboutData.js

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
-    title: "Who I am",
-    description1: "My name's Kaustubh Shelar. Currently an undergraduate in Computer Engineering, and prepared to contribute to organizational success while developing new skills and gaining real-world experience.",
-    description2: " With a focus on Data Science, Analytics, and Web Development, I possess good communication and critical thinking skills. I love to learn and explore new arenas.",
+    title: "Who am I",
+    description1: "My name's Tejas Sangale. Currently an undergraduate in Electronics and Telecommunication Engineering, and prepared to contribute to organizational success while developing new skills and gaining real-world experience.",
+    description2: " With a focus on Cloud Engineering, DevOps, and Web Development, I possess good communication and critical thinking skills. I love to learn and explore new arenas.",
     image: 2
 }
