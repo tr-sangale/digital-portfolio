5d76aee3aca07d2b6ed2ae6fdf9854fa56d36e80
tr-sangale <86203327+tr-sangale@users.noreply.github.com>
2023-05-29 15:36:21 +0530

    Update experienceData.js

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -5,14 +5,22 @@ export const experienceData = [
         jobtitle: 'Software Engineer',
         startYear: 'Jan\'2023',
         endYear: 'Present',
-        description: '● Installed and configured the 3DEXPERIENCE software by Dassault Systemes\n● Worked on a live project for the company\n● Fetched 3DX data and migrated it to H2 Database and used this data to generate Data Assessment Report in PDF format using JPO(Java) code'
+        description: '● Installed and configured the 3DEXPERIENCE software by Dassault Systemes\n● Worked on a live project for the company\n● Created automation scripts for the installation of 3DEXPERIENCE services, to reduce the installation time'
     },
     {
         id: 2,
-        company: 'UInSports',
-        jobtitle: 'Web Developer',
-        startYear: 'Sep\'2021',
+        company: 'GLogic India',
+        jobtitle: 'Cloud Engineer',
+        startYear: 'Jul\'2022',
+        endYear: 'Sept\'2022',
+        description: '● Helped the fir m to migrate from Azure to GCP , so had gain the knowledge of both the CSP (cloud service provider) ● Managing and creating containers was a one of the basic roles which i played fr the organization. ● And one of my other roles in Legitquest was to maintain and create containarized nodejs and other frameworks application'
+    },
+    {
+        id: 3,
+        company: 'GLogic India',
+        jobtitle: 'Cloud Engineer',
+        startYear: 'Aprl\'2022',
         endYear: 'Jul\'2022',
-        description: '● Created a feature as a react component for the client website, which will crop/edit an image before posting or setting up the profile picture ● Analyzed best practices and project structure in Flutter for mobile app development ● Technologies used are HTML, CSS, ReactJs, Material UI, Python Flask, Flutter ● Followed Agile Scrum methodology throughout the internship'
+        description: '● Developed CI/CD pipelines using different cicd automation tools such as jenkins and circleci ● Worked on different cloud (AWS specific) servces such as EC2, S3 bucket storage etc.'
     },
 ]
