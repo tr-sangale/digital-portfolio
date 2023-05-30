e31bef7c8c661c6b52b148bf7f91d4b68ffc5ec0
tr-sangale <86203327+tr-sangale@users.noreply.github.com>
2023-05-31 00:36:43 +0530

    Update projectsData.js

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,8 +1,8 @@
 //import three from '../assets/svg/projects/three.svg'
-import four from '../assets/svg/projects/four.svg'
-import five from '../assets/svg/projects/five.svg'
+//import four from '../assets/svg/projects/four.svg'
+//import five from '../assets/svg/projects/five.svg'
 import eight from '../assets/svg/projects/eight.svg'
-import thirteen from '../assets/svg/projects/thirteen.svg'
+//import thirteen from '../assets/svg/projects/thirteen.svg'
 import sample from '../assets/svg/projects/sample.svg'
 
 
@@ -18,48 +18,12 @@ export const projectsData = [
     },
     {
         id: 2,
-        projectName: 'Vehicle Detection, Counting, and Classification System',
-        projectDesc: 'Web application for keeping track of vehicles on the road and classifying them according to the vehicle type. It is developed using Computer Vision techniques and deployed on Streamlit cloud',
-        tags: ['Python', 'OpenCV', 'Streamlit', 'GitHub'],
-        code: 'https://github.com/KaustubhShelar/Object-Detection',
-        demo: 'https://kaustubhshelar-object-detection-main2-b5zsx7.streamlit.app/',
+        projectName: 'Helmet Detection Using ML For Safety',
+        projectDesc: 'Helmet detection for safety purpose using ML, and fine generation on the owner.',
+        tags: ['Python', 'OpenCV', 'TensorFlow', 'Anaconda'],
+        code: '',
+        demo: '',
         image: eight
-    },
-    {
-        id: 3,
-        projectName: 'Transaction Management System (Split-It)',
-        projectDesc: 'Web application for keeping track of common spending in groups. We can add transactions done in groups and get the report of the same',
-        tags: ['HTML', 'CSS', 'Javascript', 'Firebase'],
-        code: 'https://github.com/KaustubhShelar/Split-It',
-        demo: 'https://split-it.girhe.com/',
-        image: thirteen
-    },
-    {
-        id: 4,
-        projectName: 'Café Management System',
-        projectDesc: 'This application manages the information about different dishes served at the Café. This application also generates the bill for the customers in .pdf format',
-        tags: ['DotNet', 'C#', 'SQL', 'DBMS'],
-        code: 'https://github.com/KaustubhShelar/Cafe-Management-System',
-        demo: 'https://github.com/KaustubhShelar/Cafe-Management-System',
-        image: four
-    },
-    {
-        id: 5,
-        projectName: 'Student Course Management System',
-        projectDesc: 'An application built using Java, and SQL to manage the information about student data, registered courses, and courses offered by the college department',
-        tags: ['Java', 'SQL'],
-        code: 'https://github.com/KaustubhShelar/StudentCourseManagement',
-        demo: 'https://github.com/KaustubhShelar/StudentCourseManagement',
-        image: sample
-    },
-    {
-        id: 6,
-        projectName: 'Online Grocery Shop',
-        projectDesc: 'An online platform to solve the issue of grocery shopping during the pandemic situation',
-        tags: ['HTML', 'CSS', 'Javascript', 'SQL', 'PHP'],
-        code: 'https://github.com/KaustubhShelar/GroceryStore.github.io',
-        demo: 'https://github.com/KaustubhShelar/GroceryStore.github.io',
-        image: five
     },
 ]
 
